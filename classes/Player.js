73d3ae0462cd253e16397e83f42b88f08cc105fb
Michael <michael.1978@gmail.com>
2016-11-02 21:10:37 +0100

    message

--- classes/Player.js
+++ classes/Player.js
@@ -1,15 +1,34 @@
 function Player(startX, startY, health, role, id){
   // set players starting position
+
   this.x = startX;
   this.y = startY;
   // set player role (includes abilities and modifiers)
   this.role = role;
-
+  this.lastAttack = 0;
+  this.RNG = 10;
+  this.attackFrequency = 1500;
   // set players health based on role
   this.baseHealth = 100;
   this.maxHealth = this.baseHealth * role.healthModifier;
   this.health = this.maxHealth;
 
+  this.addRNG(base){
+    var multiplier = (Math.Random() * 9 + 11)/10;
+    return base * multiplier;
+  }
+
+this.attack(other){
+  if (lastAttack + attackFrequency > Date.now()){
+    other.damage(this.role.getDamageAmount(), this.role.getDamageType());
+  }
+  else{
+    console.log('you\'re fast, but not Chuck Norris');
+    // do nothing
+    // or send feedback in chat or on HUD?
+  }
+
+}
 
 // let the player take some damage
 this.damage(amount, type){
@@ -24,9 +43,19 @@ this.damage(amount, type){
 // let the player be healed
 this.heal(amount){
   this.health += amount;
+  if (this.health > this.maxHealth){
+    this.health = this.maxHealth;
+  }
 }
 
   this.move(x,y){
+
+    /*
+
+    future: if there's a path, then create it, else return some error..
+
+    but for now...
+    */
     this.x = x;
     this.y = y;
   }
